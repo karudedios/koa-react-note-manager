247b0e241a7cc47449f34172c7ed7faf112f358e
Carlos de Dios <c.dedios@outlook.com>
2018-07-29 22:29:11 +0000

    [KRNM-15] Configure React Application
    - Configuring Webpack through Express
    - Condifuring File Service for Attachments
    - Configuring Absolute bare-bones of the React Application
    - Installing all required packages

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-import Express from 'express';
+import { join } from 'path';
 import buildApi from './api';
+import { readSync } from 'fs';
+import Express from 'express';
 import mongoose from 'mongoose';
 import * as Models from './models';
 import bodyParser from 'body-parser';
@@ -22,6 +24,47 @@ Express()
     extended: true
   }))
   .use('/api', buildApi(Services, Models))
+  .get('/attachments/:name', (req, res, next) => {
+    res.status(200).sendFile(req.params.name, {
+      root: join(__dirname, '..', 'attachments')
+    }, function(err) {
+      if (err) return next(err);
+    });
+  });
+
+const isDeveloping = process.env.NODE_ENV !== 'production';
+
+if (isDeveloping) {
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('../client/webpack.config.js');
+
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'client/src',
+    stats: {
+      hash: false,
+      colors: true,
+      chunks: true,
+      timings: true,
+      modules: false,
+      chunkModules: true,
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.use(Express.static(join(__dirname, 'client', 'dist', 'index.html')));
+} else {
+  app.use(Express.static(__dirname + '/client/dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(join(__dirname, '/client/dist/index.html'));
+  });
+}
+
+app
   .listen(port, ip, () => {
     // eslint-disable-next-line
     console.log(`Server listening on ${ip}:${port}`);
